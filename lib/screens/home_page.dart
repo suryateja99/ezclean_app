2e35019ee660e3d8fb74187922ba90dd47594d14
EZClean_app <ksteja99@gmail.com>
2023-10-22 14:18:30 -0400

    kothagattus

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -143,7 +143,7 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Text(
-                      'Hi! kothagattu',
+                      'Hi! kothagattuS',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 24,
